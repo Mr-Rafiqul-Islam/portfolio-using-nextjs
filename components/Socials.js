5238723152636a7abba1144c3901c3d0a35f5811
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-09-04 00:56:08 +0600

    social link updated

--- components/Socials.js
+++ components/Socials.js
@@ -2,24 +2,28 @@
 import Link from "next/link";
 // icons
 import {
-  RiYoutubeLine,
   RiInstagramLine,
   RiFacebookLine,
   RiDribbbleLine,
   RiBehanceLine,
   RiPinterestLine,
+  RiGithubLine,
+  RiLinkedinLine,
 } from "react-icons/ri";
 
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={""} className="hover:text-accent transition-all duration-300">
-        <RiYoutubeLine />
+      <Link href={"https://www.linkedin.com/in/mr-rafiqul-islam/"} target="_blank" className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
       </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link href={"https://github.com/Mr-Rafiqul-Islam"} target="_blank" className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={"https://www.facebook.com/mr.rafiqulthedev"} target="_blank" className="hover:text-accent transition-all duration-300">
         <RiFacebookLine />
       </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link href={"https://www.instagram.com/mr.rafiqulthedev"} target="_blank" className="hover:text-accent transition-all duration-300">
         <RiInstagramLine />
       </Link>
       <Link href={""} className="hover:text-accent transition-all duration-300">
@@ -27,9 +31,6 @@ const Socials = () => {
       </Link>
       <Link href={""} className="hover:text-accent transition-all duration-300">
         <RiBehanceLine />
-      </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
-        <RiPinterestLine />
       </Link>
     </div>
   );
