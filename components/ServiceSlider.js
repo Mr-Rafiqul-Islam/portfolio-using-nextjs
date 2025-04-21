89dbd534fb766a7a7f77f207f76bd8c0d63488ff
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-04-21 11:54:48 +0600

    fix: add fixed height in service slider card

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -61,7 +61,7 @@ const ServiceSlider = () => {
     >
       {serviceData.map((service, index) => (
         <SwiperSlide key={index}>
-          <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+          <div className="bg-[rgba(65,47,123,0.15)] h-full rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
             {/* icons */}
             <div className="text-4xl text-accent mb-4">{service.icon}</div>
             {/* title & description */}
